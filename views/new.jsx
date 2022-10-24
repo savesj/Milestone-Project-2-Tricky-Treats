3390853834c618b83d8cc52c930a4d3ddc983c2b
only2pencils <89320439+only2pencils@users.noreply.github.com>
2022-10-24 19:41:52 -0400

    new views complete

---
+++ views/new.jsx
@@ -0,0 +1,41 @@
+const React = require('react')
+const Default = require('./layout/Default')
+
+function New ({recipes}) {
+    return (
+        <Default>
+            <h2> Add a new treat</h2>
+            <form action="/treats" method="POST">
+                <label htmlFor="name">Name</label>
+                <input
+                type="text"
+                name="name"
+                id="name"
+                required
+                />
+                <label htmlFor="image">Image</label>
+                <input
+                type="text"                 
+                name="image"
+                id="image"/>
+                <label htmlFor="recipe">Recipe</label>
+                <select name="recipe" id="recipe">
+                    {recipes.map((recipe) => {
+                        return(
+                            <option value={recipe.id} key={recipe.id}>{recipe.name}</option>
+                        )
+                    })}
+                </select>
+                <label htmlFor="hasGooten">Has Gooten</label>
+                <input
+                type="checkbox"
+                name="hasGooten"
+                id="hasGooten"
+                defaultChecked
+                />
+                <br />
+                <input type="submit"/>
+            </form>
+        </Default>
+    )
+}
